d146ea70605b5f49916902a5a6cb8812270bc2a5
sohwijung <sohwi.j@gmail.com>
2020-06-25 16:41:28 +0900

    Added image transition

--- includes/init.php
+++ includes/init.php
@@ -1,8 +1,9 @@
 <?php
+// check current php version to ensure it meets 2300's requirements
 function check_php_version()
 {
   if (version_compare(phpversion(), '7.0', '<')) {
-    define('VERSION_MESSAGE', "PHP version 7.0 or higher is required.");
+    define('VERSION_MESSAGE', "PHP version 7.0 or higher is required for 2300. Make sure you have installed PHP 7 on your computer and have set the correct PHP path in VS Code.");
     echo VERSION_MESSAGE;
     throw VERSION_MESSAGE;
   }
@@ -16,7 +17,6 @@ function config_php_errors()
   error_reporting(E_ALL);
 }
 config_php_errors();
-
 
 function open_sqlite_db($db_filename)
 {
